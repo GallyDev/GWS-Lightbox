092ab71de9e2e8d8ea5524dc9f7b642797ff9241
René Jossen <rene@enjor.ch>
2026-02-18 08:57:26 +0100

    Fehlerbehebung: Klasse des Lightbox-Elements von 'gws-lightbox' auf 'gws-lightbox-overlay' geändert

--- js/gws-lightbox.js
+++ js/gws-lightbox.js
@@ -6,7 +6,7 @@ if(lightboxElements.length > 0){
 	lightboxElements.forEach(gallery => {
 
 		const lightbox = document.createElement('div');
-		lightbox.className = 'gws-lightbox';
+		lightbox.className = 'gws-lightbox-overlay';
 
 		const lightboxContent = document.createElement('div');
 		lightboxContent.classList.add('lightbox-content');
